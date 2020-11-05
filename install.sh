ab3701e291ac2ef506921468781707579cf50231
yangfeng <yangfengzzz@hotmail.com>
2020-11-05 14:12:52 +0800

    delete cmake

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#compile the shared library
-echo "compile the shared library--------------------------"
-mkdir -pv build
-cd build
-cmake ..
-make -j4
-echo "finish ----------------------------------------------"
--- uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#uninstall shared lib
-rm -r build
